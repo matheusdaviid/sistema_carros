3aa9de5bbcc4ad3c7db5cfa622d27a9ecc0daf94
matheusdaviid <matheus971592191@gmail.com>
2025-02-17 11:52:57 -0300

    pronto

---
+++ Sistema2/Sitema2/Gerenciamento_Clientes.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Sitema2
+{
+    partial class Gerenciamento_Clientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+            this.buttonPesquisarClientes = new System.Windows.Forms.Button();
+            this.buttonRemoverClientes = new System.Windows.Forms.Button();
+            this.buttonFechar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvClientes
+            // 
+            this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes.Location = new System.Drawing.Point(12, 121);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.Size = new System.Drawing.Size(633, 358);
+            this.dgvClientes.TabIndex = 0;
+            // 
+            // buttonPesquisarClientes
+            // 
+            this.buttonPesquisarClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPesquisarClientes.Location = new System.Drawing.Point(726, 121);
+            this.buttonPesquisarClientes.Name = "buttonPesquisarClientes";
+            this.buttonPesquisarClientes.Size = new System.Drawing.Size(226, 47);
+            this.buttonPesquisarClientes.TabIndex = 1;
+            this.buttonPesquisarClientes.Text = "Pesquisar Clientes";
+            this.buttonPesquisarClientes.UseVisualStyleBackColor = true;
+            this.buttonPesquisarClientes.Click += new System.EventHandler(this.buttonPesquisarClientes_Click);
+            // 
+            // buttonRemoverClientes
+            // 
+            this.buttonRemoverClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRemoverClientes.Location = new System.Drawing.Point(726, 212);
+            this.buttonRemoverClientes.Name = "buttonRemoverClientes";
+            this.buttonRemoverClientes.Size = new System.Drawing.Size(226, 47);
+            this.buttonRemoverClientes.TabIndex = 2;
+            this.buttonRemoverClientes.Text = "Remover Clientes";
+            this.buttonRemoverClientes.UseVisualStyleBackColor = true;
+            // 
+            // buttonFechar
+            // 
+            this.buttonFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFechar.Location = new System.Drawing.Point(726, 432);
+            this.buttonFechar.Name = "buttonFechar";
+            this.buttonFechar.Size = new System.Drawing.Size(226, 47);
+            this.buttonFechar.TabIndex = 3;
+            this.buttonFechar.Text = "Fechar";
+            this.buttonFechar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(275, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(470, 42);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Gerenciamento de Clientes";
+            // 
+            // Gerenciamento_Clientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 551);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonFechar);
+            this.Controls.Add(this.buttonRemoverClientes);
+            this.Controls.Add(this.buttonPesquisarClientes);
+            this.Controls.Add(this.dgvClientes);
+            this.Name = "Gerenciamento_Clientes";
+            this.Text = "Gerenciamento_Clientes";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvClientes;
+        private System.Windows.Forms.Button buttonPesquisarClientes;
+        private System.Windows.Forms.Button buttonRemoverClientes;
+        private System.Windows.Forms.Button buttonFechar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Sistema2/Sitema2/Gerenciamento_Clientes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Sitema2
+{
+    public partial class Gerenciamento_Clientes : Form
+    {
+        public Gerenciamento_Clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPesquisarClientes_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
